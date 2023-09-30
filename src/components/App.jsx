76269a3abada4f6ceae50cd21a8264da2d004d1a
Vasyl Holubovskyi <66141432+holvas-web@users.noreply.github.com>
2023-09-30 23:27:44 +0200

    fixed code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 // App.js
+
+// App.js
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { nanoid } from 'nanoid';
@@ -7,22 +9,16 @@ import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { Container } from './ContactForm/ContactForm.styled';
 import { Header } from './ContactList/ContactList.styled';
-import { addContact, deleteContact, updateFilter } from '../redux/contactsSlice'; 
-import 'modern-normalize';
+import { addContact, deleteContact, updateFilter } from '../redux/contactsSlice';
 
 export const App = () => {
   const dispatch = useDispatch();
-  // const contacts = useSelector(state => state.contacts.contacts);
   const state = useSelector(state => state);
-  const filter = useSelector(state => state.contacts.filter);
-
-  console.log("Contacts:", state); 
-  console.log("Filter:", filter); 
 
   const handleAddContact = (name, number) => {
     dispatch(addContact({ id: nanoid(), name, number }));
   };
-  console.log(handleAddContact);
+
   const handleDeleteContact = contactId => {
     dispatch(deleteContact(contactId));
   };
@@ -31,8 +27,8 @@ export const App = () => {
     dispatch(updateFilter(event.target.value));
   };
 
-  const filteredContacts = state.contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
+  const filteredContacts = state.contacts.contacts.filter(contact =>
+    contact.name.toLowerCase().includes(state.contacts.filter.toLowerCase())
   );
 
   return (
@@ -40,8 +36,55 @@ export const App = () => {
       <h1>Phonebook</h1>
       <ContactForm onAddContact={handleAddContact} />
       <Header>Contacts</Header>
-      <Filter value={filter} onChange={handleFilterChange} />
+      <Filter value={state.contacts.filter} onChange={handleFilterChange} />
       <ContactList contacts={filteredContacts} onDeleteContact={handleDeleteContact} />
     </Container>
   );
 };
+
+
+// import React from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { nanoid } from 'nanoid';
+// import { ContactForm } from './ContactForm/ContactForm';
+// import { ContactList } from './ContactList/ContactList';
+// import { Filter } from './Filter/Filter';
+// import { Container } from './ContactForm/ContactForm.styled';
+// import { Header } from './ContactList/ContactList.styled';
+// import { addContact, deleteContact, updateFilter } from '../redux/contactsSlice'; 
+// import 'modern-normalize';
+
+// export const App = () => {
+//   const dispatch = useDispatch();
+//   const contacts = useSelector(state => state.contacts.contacts);
+//   const filter = useSelector(state => state.contacts.filter);
+
+//   console.log("Contacts:", contacts); 
+//   console.log("Filter:", filter); 
+
+//   const handleAddContact = (name, number) => {
+//     dispatch(addContact({ id: nanoid(), name, number }));
+//   };
+//   console.log(handleAddContact);
+//   const handleDeleteContact = contactId => {
+//     dispatch(deleteContact(contactId));
+//   };
+
+//   const handleFilterChange = event => {
+//     dispatch(updateFilter(event.target.value));
+//   };
+
+//   const filteredContacts = contacts.contacts.filter(contact =>
+//     contact.name.toLowerCase().includes(filter.toLowerCase())
+//   );
+
+//   return (
+//     <Container>
+//       <h1>Phonebook</h1>
+//       <ContactForm onAddContact={handleAddContact} />
+//       <Header>Contacts</Header>
+//       <Filter value={filter} onChange={handleFilterChange} />
+//       <ContactList contacts={filteredContacts} onDeleteContact={handleDeleteContact} />
+//     </Container>
+//   );
+// };
