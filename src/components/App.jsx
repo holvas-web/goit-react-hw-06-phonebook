146de2b79dff3546a133700c184eb0227f646a68
Vasyl Holubovskyi <66141432+holvas-web@users.noreply.github.com>
2023-10-01 21:05:01 +0200

    fixed filter & add contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,4 @@
-// App.js
-
-// App.js
+                                                                                                                                                                                                                                                                                                                     // App.js
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { nanoid } from 'nanoid';
--- src/redux/contactsSlice.jsx
+++ src/redux/contactsSlice.jsx
@@ -6,7 +6,7 @@ const contactsSlice = createSlice({
   initialState: { contacts: [], filter: '' },
   reducers: {
     addContact: (state, action) => {
-      state.contacts.push(action.payload);
+      state.push(action.payload);
     },
     deleteContact: (state, action) => {
       state.contacts = state.contacts.filter(contact => contact.id !== action.payload);
