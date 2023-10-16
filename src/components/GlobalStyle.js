2c2fe2cf79fc2144e896739325bfd60d0028926c
Vasyl Holubovskyi <66141432+holvas-web@users.noreply.github.com>
2023-10-16 13:24:42 +0200

    add global styles

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,61 +1,53 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 import 'modern-normalize';
 
-export const GlobalStyle = createGlobalStyle `
-    body {
-        margin: 0;
-        
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-    }
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    min-height: 100%;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
 
-    code {
-        font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-        monospace;
-    }
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 
-    ul {
-        margin: 0;
-        padding: 0;
-        list-style: none;
+    // background: linear-gradient(211deg, rgba(75,20,20,1) 0%, rgba(224,171,173,1) 39%, rgba(216,163,165,1) 57%, rgba(253,215,215,1) 73%, rgba(255,255,255,1) 100%) no-repeat fixed;
 
-        font-family: math, sans-serif; //'Segoe UI'
-        line-height: 1.10;
-        letter-spacing: 0.2px;
-    }
+    background-image: url(https://e0.pxfuel.com/wallpapers/686/271/desktop-wallpaper-nature-berries-plant-macro-rowan.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+  }
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
 
-    li::marker {
-        padding: 0;
-    }
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+	margin: 0;
+}
 
-    h1, h2 {
-        padding-top: 30px;
-    }
-    p {
-        padding: 0;
+ul {
+	margin: 0;
+	padding: 0;
+	list-style: none;
+}
 
-        font-family: 'Roboto', 'Oxygen', sans-serif;
-        line-height: 1.10;
-        letter-spacing: 0.2px;
-    }
+img {
+	display: block;
+	max-width: 100%;
+	height: auto;
+}
 
-    button {
-        display: flex;
-        justify-content: center;
-        align-items: center;
+a {
+text-decoration: none;
+}
 
-        background-color: yellow;
-        padding: 10px 30px;
-
-        border-radius: 4px;
-        box-shadow: 0px 2px 6px 0px lawngreen;
-        border: 1px solid lawngreen;
-
-        transition: all 300ms ease-in-out;
-
-        &:hover {
-            transform: scale(1.03);
-            background-color: #09f;
-        }
-    }
-`
+`;
