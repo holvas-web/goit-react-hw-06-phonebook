d638236129f37bd3a308f2c4fe7849a12c05e8cf
Vasyl Holubovskyi <66141432+holvas-web@users.noreply.github.com>
2023-10-19 15:07:54 +0200

    233124г2343х

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,17 +1,12 @@
-import { createSelector } from '@reduxjs/toolkit';
-// import { store } from "./store";
-// import {toast} from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-
+// import { createSelector } from '@reduxjs/toolkit';
+import { store } from "./store"
 
 export const getContacts = store => store.contacts.items;
 
 export const getFilter = store => store.filter;
 
-export const getFilteredContacts = createSelector([getContacts, getFilter],(contacts, filter)  => {
-
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-}
-);
+export const getFilteredContacts = store => {
+  return store.contacts.items.filter(contact =>
+    contact.name.toLowerCase().includes(store.filter.toLowerCase())
+  )
+};
